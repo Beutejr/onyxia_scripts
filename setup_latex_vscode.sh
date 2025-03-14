e891c9055d77509013111bfacfc03cef64fd922c
Beutejr <63194251+Beutejr@users.noreply.github.com>
2025-03-14 17:11:53 +0100

    Update setup_latex_vscode.sh

--- setup_latex_vscode.sh
+++ setup_latex_vscode.sh
@@ -10,6 +10,15 @@
 code-server --install-extension James-Yu.latex-workshop
 # german spell checker
 code-server --install-extension streetsidesoftware.code-spell-checker-german
+
+# update tex-live
+URL="https://mirror.ctan.org/systems/texlive/tlnet/update-tlmgr-latest.sh"
+TEMP_DIR="/tmp"
+SCRIPT_NAME="update-tlmgr-latest.sh"
+SCRIPT_PATH="$TEMP_DIR/$SCRIPT_NAME"
+curl -o "$SCRIPT_PATH" "$URL"
+chmod +x "$SCRIPT_PATH"
+sh "$SCRIPT_PATH" -- --upgrade
 # latex packages
 tlmgr install epigraph emptypage bibtopic nextpage babel-german hyphen-german
 
